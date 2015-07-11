0817591c37dbec9412d585a98afe876c1db7cf49
Jordan DE GEA <jordan.degea@gmail.com>
2015-07-11 17:54:05 +0200

    Clés SSH pour swiftmailer

--- src/Sinenco/CoreBundle/Services/MailerService.php
+++ src/Sinenco/CoreBundle/Services/MailerService.php
@@ -9,33 +9,34 @@ use Doctrine\ORM\EntityManager,
 
 class MailerService {
 
-    public function sendMail(Container $controller, $subject, $address, $plain, $params = null, $renderPlain = false, $html = null,  $renderHTML = false ) {
+    public function sendMail(Container $controller, $subject, $address, $plain, $params = null, $renderPlain = false, $html = null, $renderHTML = false) {
+        $smimeSigner = \Swift_Signers_SMimeSigner::newInstance();
+        $smimeSigner->setSignCertificate('/etc/apache/server.csr', array('/etc/apache/myserver.key', 'Valkyrie85#'));
+
         $message = \Swift_Message::newInstance()
                 ->setSubject($subject)
                 ->setFrom('webmaster@sinenco.com')
                 ->setTo($address)
+                ->attachSigner($smimeSigner);
         ;
 
         if ($renderPlain) {
             $message->setBody(
-                    $controller->get('templating')->render($plain, $params), 
-                    'text/plain'
+                    $controller->get('templating')->render($plain, $params), 'text/plain'
             );
-        }else{
+        } else {
             $message->setBody($plain, 'text/plain');
         }
- 
 
-        if ( $html != null ){
+
+        if ($html != null) {
             if ($renderHTML) {
                 $message->setBody(
-                        $controller->get('templating')->render($html, $params), 
-                        'text/html'
+                        $controller->get('templating')->render($html, $params), 'text/html'
                 );
             } else {
                 $message->setBody(
-                        $controller->get('templating')->render($html, $params), 
-                        'text/html');
+                        $controller->get('templating')->render($html, $params), 'text/html');
             }
         }
         $controller->get('mailer')->send($message);
