7c175b10d086f19b4c1639a62fbc3f9977f9c9fa
Jordan DE GEA <jordan.degea@gmail.com>
2016-01-08 09:08:46 +0100

    Ajout de Doctrine Migration

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -12,6 +12,7 @@ class AppKernel extends Kernel
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
 
             
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
